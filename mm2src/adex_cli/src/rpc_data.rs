ffa086dd29bfe9ecaec1125cf22525128d03453c
Omer Yacine <mariocynicys@gmail.com>
2024-06-03 15:44:35 +0200

    fix adex-cli compilation issue

--- mm2src/adex_cli/src/rpc_data.rs
+++ mm2src/adex_cli/src/rpc_data.rs
@@ -3,7 +3,7 @@
 //! *Note: it's expected that the following data types will be moved to mm2_rpc::data when mm2 is refactored to be able to handle them*
 //!
 
-use mm2_rpc::data::legacy::{ElectrumProtocol, GasStationPricePolicy, Priority, UtxoMergeParams};
+use mm2_rpc::data::legacy::{ElectrumProtocol, Priority, UtxoMergeParams};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
