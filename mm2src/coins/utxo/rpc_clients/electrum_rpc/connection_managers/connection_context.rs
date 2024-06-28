0103f0b2ece3e34ab50c56378b891f8ab847082e
Omer Yacine <mariocynicys@gmail.com>
2024-06-28 13:29:04 +0200

    package `disconnected_at` and `next_suspend_time` in one mutex
    These two pieces of info are always used & updated together, put them
    into a single mutex.

--- mm2src/coins/utxo/rpc_clients/electrum_rpc/connection_managers/connection_context.rs
+++ mm2src/coins/utxo/rpc_clients/electrum_rpc/connection_managers/connection_context.rs
@@ -8,6 +8,41 @@ use super::super::constants::FIRST_SUSPEND_TIME;
 use common::now_ms;
 use keys::Address;
 
+#[derive(Debug)]
+struct SuspendTimer {
+    /// How long to suspend the server the next time it disconnects (in milliseconds).
+    next_suspend_time: u64,
+    /// When was the connection last disconnected.
+    disconnected_at: u64,
+}
+
+impl SuspendTimer {
+    /// Creates a new suspend timer.
+    fn new() -> Self {
+        SuspendTimer {
+            next_suspend_time: FIRST_SUSPEND_TIME,
+            disconnected_at: 0,
+        }
+    }
+
+    /// Resets the suspend time and disconnection time.
+    fn reset(&mut self) {
+        self.disconnected_at = 0;
+        self.next_suspend_time = FIRST_SUSPEND_TIME;
+    }
+
+    /// Doubles the suspend time and sets the disconnection time to `now`.
+    fn double(&mut self) {
+        // The max suspend time, 12h.
+        const MAX_SUSPEND_TIME: u64 = 12 * 60 * 60;
+        self.disconnected_at = now_ms();
+        self.next_suspend_time = (self.next_suspend_time * 2).min(MAX_SUSPEND_TIME);
+    }
+
+    /// Returns the time until when the server should be suspended in milliseconds.
+    fn get_suspend_until(&self) -> u64 { self.disconnected_at + self.next_suspend_time * 1000 }
+}
+
 /// A struct that encapsulates an Electrum connection and its information.
 #[derive(Debug)]
 pub struct ConnectionContext {
@@ -15,10 +50,8 @@ pub struct ConnectionContext {
     pub connection: Arc<ElectrumConnection>,
     /// The list of addresses subscribed to the connection.
     subs: Mutex<HashSet<Address>>,
-    /// How long to suspend the server the next time it disconnects (in milliseconds).
-    next_suspend_time: Mutex<u64>,
-    /// When was the connection last disconnected.
-    disconnected_at: Mutex<u64>,
+    /// The timer deciding when the connection is ready to be used again.
+    suspend_timer: Mutex<SuspendTimer>,
 }
 
 impl ConnectionContext {
@@ -27,34 +60,23 @@ impl ConnectionContext {
         ConnectionContext {
             connection: Arc::new(connection),
             subs: Mutex::new(HashSet::new()),
-            next_suspend_time: Mutex::new(FIRST_SUSPEND_TIME),
-            disconnected_at: Mutex::new(0),
+            suspend_timer: Mutex::new(SuspendTimer::new()),
         }
     }
 
-    /// Resets the suspend time and disconnection time.
-    pub(super) fn connected(&self) {
-        *self.disconnected_at.lock().unwrap() = 0;
-        *self.next_suspend_time.lock().unwrap() = FIRST_SUSPEND_TIME;
-    }
+    /// Resets the suspend time.
+    pub(super) fn connected(&self) { self.suspend_timer.lock().unwrap().reset(); }
 
     /// Inform the connection context that the connection has been disconnected.
     ///
-    /// Doubles the suspend time and sets the disconnection time to `now`.
-    /// Also clears the subs list and returns it.
+    /// Doubles the suspend time and clears the subs list and returns it.
     pub(super) fn disconnected(&self) -> HashSet<Address> {
-        // The max time to suspend a server, 12h.
-        const MAX_SUSPEND_TIME: u64 = 12 * 60 * 60;
-        *self.disconnected_at.lock().unwrap() = now_ms();
-        let mut next_suspend_time = self.next_suspend_time.lock().unwrap();
-        *next_suspend_time = (*next_suspend_time * 2).min(MAX_SUSPEND_TIME);
+        self.suspend_timer.lock().unwrap().double();
         mem::take(&mut self.subs.lock().unwrap())
     }
 
     /// Returns the time the server should be suspended until (when to take it up) in milliseconds.
-    pub(super) fn suspend_until(&self) -> u64 {
-        *self.disconnected_at.lock().unwrap() + *self.next_suspend_time.lock().unwrap() * 1000
-    }
+    pub(super) fn suspend_until(&self) -> u64 { self.suspend_timer.lock().unwrap().get_suspend_until() }
 
     /// Adds a subscription to the connection context.
     pub(super) fn add_sub(&self, address: Address) { self.subs.lock().unwrap().insert(address); }
