395587e45847749228dd9a3d75082ee843ce412f
matevszm <mateusz.marcinkowski@blockydevs.com>
2024-09-24 11:41:47 +0200

    feat: Key derivation with solana path

--- mm2src/coins/solana/solana_hd_wallet.rs
+++ mm2src/coins/solana/solana_hd_wallet.rs
@@ -1,9 +1,10 @@
+use std::str::FromStr;
 use crate::hd_wallet::{HDAccount, HDAddress, HDWallet};
 use bip32::{ExtendedPublicKey, PrivateKeyBytes, PublicKey as bip32PublicKey, PublicKeyBytes, Result as bip32Result};
 use web3::types::H256;
 use ed25519_dalek::{PublicKey as Ed25519PublicKey};
 use ed25519_dalek::SECRET_KEY_LENGTH;
-use ed25519_dalek_bip32::{ChildIndex, ExtendedSecretKey, SecretKey};
+use ed25519_dalek_bip32::{ChildIndex, ExtendedSecretKey, SecretKey, DerivationPath};
 
 pub struct Address(pub H256);
 
@@ -14,15 +15,7 @@ pub type SolanaHDAccount = HDAccount<SolanaHDAddress, Ed25519ExtendedPublicKey>;
 pub type SolanaHDWallet = HDWallet<SolanaHDAccount>;
 pub type Ed25519ExtendedPublicKey = ExtendedPublicKey<SolanaPublicKey>;
 
-impl bip32PublicKey for SolanaPublicKey {
-    fn from_bytes(_bytes: PublicKeyBytes) -> bip32Result<Self> {
-        todo!()
-    }
-
-    fn to_bytes(&self) -> PublicKeyBytes {
-        todo!()
-    }
-
+impl SolanaPublicKey {
     fn derive_child(&self, _other: PrivateKeyBytes) -> bip32Result<Self> {
         todo!()
         // derive_ed25519_key_impl()
@@ -36,18 +29,19 @@ const SEED_BYTES: [u8; SECRET_KEY_LENGTH] = [
     112, 059, 172, 003, 028, 174, 127, 096, ];
 
 fn derive_ed25519_key_impl(seed: [u8; SECRET_KEY_LENGTH], index: u32) -> ExtendedSecretKey {
-   let xprv = ExtendedSecretKey::from_seed(&seed).unwrap();
-   xprv.derive_child(ChildIndex::Hardened(index)).unwrap()
+    let xprv = ExtendedSecretKey::from_seed(&seed).unwrap();
+    let derivation_path: DerivationPath = format!("m/44'/501'/{index}'/0'").parse().unwrap();
+    xprv.derive(&derivation_path).unwrap()
 }
 
 #[test]
 fn key_derivation() {
     let key_0 = derive_ed25519_key_impl(SEED_BYTES, 0);
-    assert_eq!(key_0.public_key().to_bytes(), [180, 84, 70, 20, 219, 166, 136, 38, 57, 179, 37, 251, 94, 32, 84, 64, 188, 87, 96, 252, 5, 86, 155, 111, 71, 97, 94, 6, 138, 166, 233, 199]);
+    assert_eq!(key_0.public_key().to_bytes(), [185, 72, 208, 205, 67, 189, 80, 35, 221, 53, 202, 150, 255, 22, 196, 170, 5, 206, 243, 76, 158, 120, 156, 107, 141, 88, 246, 31, 168, 83, 93, 129]);
 
     let key_5 = derive_ed25519_key_impl(SEED_BYTES, 5);
-    assert_eq!(key_5.public_key().to_bytes(), [218, 76, 229, 83, 128, 102, 49, 238, 89, 71, 118, 37, 190, 119, 112, 19, 203, 182, 255, 143, 241, 152, 222, 45, 4, 229, 94, 2, 141, 46, 21, 13]);
+    assert_eq!(key_5.public_key().to_bytes(), [126, 99, 126, 54, 130, 205, 203, 86, 182, 109, 13, 40, 54, 190, 38, 100, 105, 43, 48, 247, 162, 35, 7, 32, 48, 146, 131, 155, 182, 245, 87, 163]);
 
     let key_8 = derive_ed25519_key_impl(SEED_BYTES, 8);
-    assert_eq!(key_8.public_key().to_bytes(), [62, 24, 165, 95, 240, 247, 73, 76, 135, 225, 82, 53, 10, 111, 218, 250, 93, 28, 203, 83, 219, 46, 228, 173, 230, 238, 84, 119, 249, 153, 29, 99]);
+    assert_eq!(key_8.public_key().to_bytes(), [185, 199, 244, 67, 196, 210, 20, 107, 189, 37, 133, 150, 248, 200, 167, 183, 88, 240, 178, 220, 41, 203, 19, 240, 198, 143, 175, 56, 216, 17, 64, 144]);
 }
